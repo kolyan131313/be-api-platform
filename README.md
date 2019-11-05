55fb12ebc96043c7bd3a6aff01915ab488cf1353
Kornyenko Nick <nick_korn@mail.ua>
2019-11-05 09:02:30 +0200

    Add readme

--- README.md
+++ README.md
@@ -1,15 +1,30 @@
 #Run local server
 symfony server:start -d
 
+#Run composer
+`composer install` command in root of project
+
+#Api location
+http://127.0.0.1:8000/api
+
+#create database
+Need to create database for application
+
+#Copy .env to .local.env and add following info
+MAILER_URL=gmail://####:#####@localhost
+DATABASE_URL=postgresql://postgres:####@127.0.0.1:5432/be_api_platform?&charset=UTF-8
+
 #Run migrations
-./bin/console doctrine:migrations:migrate
+`./bin/console doctrine:migrations:migrate`
 
 #Load test users of different roles
-php bin/console doctrine:fixtures:load
+`php bin/console doctrine:fixtures:load`
 
 #Database create database
 Create databases for local environment and test environment
-for example: be_api_platform
+for example: be_api_platform (postgres|mysql) in .env.local file
 
 #Run tests
 ./bin/console doctrine:migrations:migrate --env=test --dry-run
+php vendor/bin/simple-phpunit
+
--- src/Migrations/Version20191105070104.php
+++ src/Migrations/Version20191105070104.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20191102214601 extends AbstractMigration
+final class Version20191105070104 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,12 +22,16 @@ final class Version20191102214601 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
+        $this->addSql('CREATE SEQUENCE "user_id_seq" INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE verification_request_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE media_object_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE post_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE TABLE "user" (id INT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, first_name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649E7927C74 ON "user" (email)');
         $this->addSql('COMMENT ON COLUMN "user".roles IS \'(DC2Type:json_array)\'');
-        $this->addSql('CREATE TABLE verification_request (id INT NOT NULL, image_id INT DEFAULT NULL, owner_id INT NOT NULL, status SMALLINT NOT NULL, message VARCHAR(255) DEFAULT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE verification_request (id INT NOT NULL, image_id INT DEFAULT NULL, owner_id INT NOT NULL, status SMALLINT NOT NULL, message VARCHAR(255) DEFAULT NULL, rejection_reason VARCHAR(255) DEFAULT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_20FDDF4E3DA5256D ON verification_request (image_id)');
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_20FDDF4E7E3C61F9 ON verification_request (owner_id)');
+        $this->addSql('CREATE INDEX IDX_20FDDF4E7E3C61F9 ON verification_request (owner_id)');
         $this->addSql('CREATE TABLE media_object (id INT NOT NULL, file_path VARCHAR(255) DEFAULT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE TABLE post (id INT NOT NULL, owner_id INT NOT NULL, title VARCHAR(255) NOT NULL, content TEXT NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_5A8A6C8D7E3C61F9 ON post (owner_id)');
@@ -45,6 +49,10 @@ final class Version20191102214601 extends AbstractMigration
         $this->addSql('ALTER TABLE verification_request DROP CONSTRAINT FK_20FDDF4E7E3C61F9');
         $this->addSql('ALTER TABLE post DROP CONSTRAINT FK_5A8A6C8D7E3C61F9');
         $this->addSql('ALTER TABLE verification_request DROP CONSTRAINT FK_20FDDF4E3DA5256D');
+        $this->addSql('DROP SEQUENCE "user_id_seq" CASCADE');
+        $this->addSql('DROP SEQUENCE verification_request_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE media_object_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE post_id_seq CASCADE');
         $this->addSql('DROP TABLE "user"');
         $this->addSql('DROP TABLE verification_request');
         $this->addSql('DROP TABLE media_object');
